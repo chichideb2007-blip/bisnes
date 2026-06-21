d7bf317d97745661c913ddb9c537914a799838d9
chichideb2007-blip <chichideb2007@gmail.com>
2026-06-21 23:41:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,7 @@ def add_data():
         return f"حدث خطأ: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
