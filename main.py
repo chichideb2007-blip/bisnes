2f4ad8163f666c36ae4486f83f035e5d33f4cc29
chichideb2007-blip <chichideb2007@gmail.com>
2026-06-21 20:07:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def add_data():
     try:
         user_name = request.args.get('username', 'شيمو')
-        # الآن بما أن الأعمدة تقبل Null، يمكننا إرسال username فقط
+        # الآن نرسل فقط 'username' لأن بقية الأعمدة تقبل Null
         data = supabase.table("users").insert({"username": user_name}).execute()
         return "تمت إضافة المستخدم بنجاح!"
     except Exception as e:
