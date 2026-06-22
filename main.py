a788c4bdfe9365e1d1a761857a3bcd604dbca4d2
chichideb2007-blip <chichideb2007@gmail.com>
2026-06-22 18:13:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from supabase import create_client
 
 app = Flask(__name__)
-app.secret_key = 'chaima_secure_2026'
+app.secret_key = 'chaima_secret_key_2026'
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
@@ -14,7 +14,6 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        # تحقق من البيانات في سوبابايس
         user = supabase.table("users").select("*").eq("username", username).eq("password", password).execute()
         if user.data:
             session['user'] = username
