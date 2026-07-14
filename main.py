0681b32e2a7b3a68e341d2152d07d91e2bc69858
chichideb2007-blip <chichideb2007@gmail.com>
2026-07-14 21:23:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello():
-    return "الموقع يعمل بنجاح!"
+def home():
+    return "الموقع يعمل بنجاح! - <a href='/login'>اذهب لصفحة الدخول</a>"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
